d3f4228df2306191ac97739b3a09d09902edbe83
Dean Gardiner <me@dgardiner.net>
2016-09-19 10:54:19 +1200

    Improved logging of progress events

--- src/modules/background/scrobble/index.js
+++ src/modules/background/scrobble/index.js
@@ -11,7 +11,19 @@ export class Scrobble {
     }
 
     onProgress(session) {
-        console.log('onProgress', session);
+        var item = session.item;
+
+        // Retrieve source key
+        var sourceKey = item.source.id.substring(item.source.id.lastIndexOf('.') + 1);
+
+        // Log status message
+        if(item.type === 'movie') {
+            console.log('[' + sourceKey + ': ' + item.id + '] ' + item.title + ' (' + item.year + ') [progress: ' + Math.round(session.progress * 100) + ']');
+        } else if(item.type === 'episode') {
+            console.log('[' + sourceKey + ': ' + item.id + '] ' + item.show.title + ' ' + item.season.number + 'x' + item.number + ' [progress: ' + Math.round(session.progress * 100) + ']');
+        } else if(item.type === 'track') {
+            console.log('[' + sourceKey + ': ' + item.id + '] ' + item.title + ' - ' + item.artist.title + ' [progress: ' + Math.round(session.progress * 100) + ']');
+        }
     }
 }
 
