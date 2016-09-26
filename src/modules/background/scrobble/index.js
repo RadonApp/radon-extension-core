f7490b20b204b6fbb9eff5a9383b15e585b54da2
Dean Gardiner <me@dgardiner.net>
2016-09-26 15:58:44 +1300

    Implemented scrobble service emitter

--- src/modules/background/scrobble/index.js
+++ src/modules/background/scrobble/index.js
@@ -1,28 +1,33 @@
+import {MediaTypes} from 'eon.extension.framework/core/enums';
 import Bus from 'eon.extension.framework/core/bus';
+import Registry from 'eon.extension.framework/core/registry';
 
 
 export class Scrobble {
     constructor() {
+        // Retrieve scrobble services, group by supported media
+        this.services = this._getServices();
+
         // Configure event bus
         Bus.configure('background/scrobble');
 
         // Events
-        Bus.on('activity.created', (session) => this._onActivityUpdate('created', session));
-        Bus.on('activity.started', (session) => this._onActivityUpdate('started', session));
-        Bus.on('activity.progress', (session) => this._onActivityUpdate('progress', session));
-        Bus.on('activity.paused', (session) => this._onActivityUpdate('paused', session));
-        Bus.on('activity.ended', (session) => this._onActivityUpdate('ended', session));
+        Bus.on('activity.created', (session) => this.onSessionUpdated('created', session));
+        Bus.on('activity.started', (session) => this.onSessionUpdated('started', session));
+        Bus.on('activity.progress', (session) => this.onSessionUpdated('progress', session));
+        Bus.on('activity.paused', (session) => this.onSessionUpdated('paused', session));
+        Bus.on('activity.ended', (session) => this.onSessionUpdated('ended', session));
     }
 
-    _onActivityUpdate(event, session) {
+    onSessionUpdated(event, session) {
         var item = session.item;
 
         // Retrieve source key
         var sourceKey = item.source.id.substring(item.source.id.lastIndexOf('.') + 1);
 
         // Log session status
-        if(item.type === 'movie') {
-            console.debug(
+        if(item.type.media === MediaTypes.Video.Movie) {
+            console.log(
                 '%o (%d) (%s: %o) : [event: %o, state: %o, progress: %o]',
                 item.title,
                 item.year,
@@ -36,8 +41,8 @@ export class Scrobble {
                 session.state,
                 Math.round(session.progress * 100)
             );
-        } else if(item.type === 'episode') {
-            console.debug(
+        } else if(item.type.media === MediaTypes.Video.Episode) {
+            console.log(
                 '%o - Season %d (%d) - Episode %d (%s: %o) : [event: %o, state: %o, progress: %o]',
                 item.show.title,
                 item.season.number,
@@ -53,8 +58,8 @@ export class Scrobble {
                 session.state,
                 Math.round(session.progress * 100)
             );
-        } else if(item.type === 'track') {
-            console.debug(
+        } else if(item.type.media === MediaTypes.Music.Track) {
+            console.log(
                 '%o - %o (%s: %o) : [event: %o, state: %o, progress: %o]',
                 // Name
                 item.artist.title,
@@ -70,7 +75,65 @@ export class Scrobble {
                 Math.round(session.progress * 100)
             );
         }
+
+        // Emit event to services
+        var services = this.services[item.type.media];
+
+        if(typeof services === 'undefined') {
+            console.log('No services available for media: %o', item.type.media);
+            return;
+        }
+
+        for(var i = 0; i < services.length; ++i) {
+            var service = services[i];
+
+            if(!service.plugin.enabled) {
+                console.log('Plugin %o is disabled', service.plugin.id);
+                continue;
+            }
+
+            if(!service.enabled) {
+                console.log('Service %o is disabled', service.id);
+                continue;
+            }
+
+            service.onSessionUpdated(event, session);
+        }
     }
+
+    // region Private methods
+
+    _getServices() {
+        var result = {};
+
+        // Iterate over scrobble services
+        var services = Registry.listServices('destination/scrobble', {
+            disabled: true
+        });
+
+        for(var i = 0; i < services.length; ++i) {
+            var service = services[i];
+
+            if(typeof service.accepts === 'undefined' || service.accepts === null) {
+                console.warn('Service %o has an invalid value specified for the "accepts" property', service.id);
+                continue;
+            }
+
+            for(var j = 0; j < service.accepts.length; ++j) {
+                var media = service.accepts[j];
+
+                if(typeof result[media] === 'undefined') {
+                    result[media] = [];
+                }
+
+                result[media].push(service);
+            }
+        }
+
+        return result;
+    }
+
+    // region endregion
 }
 
 export default new Scrobble();
