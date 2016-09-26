837d8a55ed070d113fcca16785891602a747d0b2
Dean Gardiner <me@dgardiner.net>
2016-09-26 15:57:58 +1300

    Added `ReactPerf`

--- src/modules/configuration/index.js
+++ src/modules/configuration/index.js
@@ -1,12 +1,15 @@
 import {Permissions} from 'eon.extension.browser';
 
 import React from 'react'
+import ReactPerf from 'react-addons-perf';
 import ReactDOM from 'react-dom'
 import {Router, hashHistory} from 'react-router'
 
 import {routes} from './routes';
 import './index.scss';
 
+
+window.ReactPerf = ReactPerf;
 
 // Initialize React
 ReactDOM.render(
