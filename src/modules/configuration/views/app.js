1a1ad284a588ff3cb677083934942b33b2ef700a
Dean Gardiner <me@dgardiner.net>
2016-09-26 15:57:05 +1300

    Renamed the "App" view to "app", and improved the performance of sidebar rendering

--- src/modules/configuration/views/app.js
+++ src/modules/configuration/views/app.js
@@ -6,7 +6,39 @@ import {Link} from 'react-router';
 
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            destinations: [],
+            sources: []
+        };
+    }
+
     componentWillMount() {
+        console.timeStamp('App.componentWillMount()');
+
+        // Update state
+        this.setState({
+            // Map destinations
+            destinations: this.listPlugins('destination').map(function(plugin) {
+                return (
+                    <li key={plugin.id} className={plugin.enabled ? '' : 'disabled'}>
+                        <Link to={'/destinations/' + plugin.id}>{plugin.title}</Link>
+                    </li>
+                );
+            }),
+
+            // Map sources
+            sources: this.listPlugins('source').map(function(plugin) {
+                return (
+                    <li key={plugin.id} className={plugin.enabled ? '' : 'disabled'}>
+                        <Link to={'/sources/' + plugin.id}>{plugin.title}</Link>
+                    </li>
+                );
+            })
+        });
+
         // Retrieve callback details
         Callbacks.get().then((callback) => {
             if(callback === null) {
@@ -22,8 +54,10 @@ export default class App extends React.Component {
     }
 
     render() {
+        console.timeStamp('App.render()');
+
         return (
-            <app>
+            <app data-view="eon.extension.core:app">
                 <div className="top-bar">
                     <div className="top-bar-left">
                         <ul className="menu">
@@ -33,31 +67,19 @@ export default class App extends React.Component {
                 </div>
 
                 <div className="expanded row">
-                    <div id="app" className="medium-9 large-10 medium-push-3 large-push-2 columns">
+                    <div id="app" className="small-11 medium-9 large-10 small-push-1 medium-push-3 large-push-2 columns">
                         {this.props.children}
                     </div>
 
-                    <div id="navigation" className="medium-3 large-2 medium-pull-9 large-pull-10 columns">
+                    <div id="navigation" className="small-1 medium-3 large-2 small-pull-11 medium-pull-9 large-pull-10 columns">
                         <ul className="vertical menu">
                             <li><Link to="/general">General</Link></li>
 
                             <li className="title-link"><Link to="/destinations">Destinations</Link></li>
-                            {this.listPlugins('destination').map(function(plugin) {
-                                return (
-                                    <li key={plugin.id} className={plugin.enabled ? '' : 'disabled'}>
-                                        <Link to={'/destinations/' + plugin.id}>{plugin.title}</Link>
-                                    </li>
-                                );
-                            })}
+                            {this.state.destinations}
 
                             <li className="title-link"><Link to="/sources">Sources</Link></li>
-                            {this.listPlugins('source').map(function(plugin) {
-                                return (
-                                    <li key={plugin.id} className={plugin.enabled ? '' : 'disabled'}>
-                                        <Link to={'/sources/' + plugin.id}>{plugin.title}</Link>
-                                    </li>
-                                );
-                            })}
+                            {this.state.sources}
                         </ul>
                     </div>
                 </div>
