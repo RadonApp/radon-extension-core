1941a8dec6f38037503db4d8c64c1a3bf01eceda
Dean Gardiner <me@dgardiner.net>
2016-10-06 18:45:14 +1300

    Added message relay module

---
+++ src/modules/background/relay/index.js
@@ -0,0 +1,14 @@
+import {isDefined} from 'eon.extension.framework/core/helpers';
+import MessagingBus, {ContextTypes} from 'eon.extension.framework/messaging/bus';
+
+
+export class Relay {
+    constructor() {
+        // Construct messaging bus
+        this.bus = new MessagingBus('eon.extension.core:relay', {
+            context: ContextTypes.Background
+        });
+    }
+}
+
+export default new Relay();
