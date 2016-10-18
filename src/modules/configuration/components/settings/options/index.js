8b00df2cb78d35d742ba1f109107655f0606b37b
Dean Gardiner <me@dgardiner.net>
2016-10-19 10:54:02 +1300

    Implemented select/drop-down configuration option component

--- src/modules/configuration/components/settings/options/index.js
+++ src/modules/configuration/components/settings/options/index.js
@@ -1,10 +1,13 @@
 import {default as _CheckboxComponent} from './checkbox';
 import {default as _EnableComponent} from './enable';
+import {default as _SelectComponent} from './select';
 
 export {_CheckboxComponent as CheckboxComponent};
 export {_EnableComponent as EnableComponent};
+export {_SelectComponent as SelectComponent};
 
 export default {
     checkbox: _CheckboxComponent,
-    enable: _EnableComponent
+    enable: _EnableComponent,
+    select: _SelectComponent
 };
---
+++ src/modules/configuration/components/settings/options/select.js
@@ -0,0 +1,86 @@
+import Preferences from 'eon.extension.browser/preferences';
+
+import {OptionComponent} from 'eon.extension.framework/services/configuration/components';
+
+import React from 'react';
+
+
+export default class SelectComponent extends OptionComponent {
+    constructor() {
+        super();
+
+        this.state = {
+            id: null,
+            current: null
+        };
+    }
+
+    componentWillMount() {
+        this.refresh(this.props.id);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.refresh(nextProps.id);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(nextProps.id !== this.state.id) {
+            return true;
+        }
+
+        if(nextState.current !== this.state.current) {
+            return true;
+        }
+
+        return false;
+    }
+
+    refresh(id) {
+        // Retrieve option state
+        Preferences.getString(id).then((current) => {
+            this.setState({
+                id: id,
+                current: current
+            });
+        });
+    }
+
+    onChanged(event) {
+        let current = event.target.value;
+
+        // Update option state
+        Preferences.putString(this.props.id, current).then(() => {
+            this.setState({
+                current: current
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div data-component="eon.extension.core:settings.options.select" className="option option-select">
+                <label htmlFor={this.props.id} style={{fontSize: 14}}>{this.props.label}</label>
+
+                <select id={this.props.id} onChange={this.onChanged.bind(this)} value={this.state.current || ''}>
+                    {this.props.options.choices.map((choice) => {
+                        return <option key={choice.key} value={choice.key}>{choice.label}</option>;
+                    })}
+                </select>
+
+                {this.props.options.summary && <div className="summary" style={{color: '#999', fontSize: 14}}>
+                    {this.props.options.summary}
+                </div>}
+            </div>
+        );
+    }
+}
+
+SelectComponent.defaultProps = {
+    id: null,
+    label: null,
+
+    options: {
+        choices: [],
+        summary: null
+    }
+};
