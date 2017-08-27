79d94e26342e10bce59f35013d8297a6af8b5744
Dean Gardiner <me@dgardiner.net>
2017-08-27 16:22:38 +1200

    Renamed the "Developer" options group to "Debugging"

--- src/services/configuration/options.js
+++ src/services/configuration/options.js
@@ -9,8 +9,8 @@ import Plugin from '../../core/plugin';
 
 export default [
     new Page(Plugin, 'general', 'General', [
-        new Group(Plugin, 'general.developer', 'Developer', [
-            new SelectOption(Plugin, 'general.developer.log_level', 'Log Level', [
+        new Group(Plugin, 'general.debugging', 'Debugging', [
+            new SelectOption(Plugin, 'general.debugging.log_level', 'Log Level', [
                 {key: 'error', label: 'Error'},
                 {key: 'warning', label: 'Warning'},
                 {key: 'notice', label: 'Notice'},
