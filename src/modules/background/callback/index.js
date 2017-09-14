08022b43db2db44526ad1115ae56009b8ae49fe2
Dean Gardiner <me@dgardiner.net>
2017-09-14 15:23:43 +1200

    Added callback redirection module (for Firefox 51 - 55)
    Workaround for callback redirection bug which breaks authentication on Firefox 51 - 55.

---
+++ src/modules/background/callback/index.js
@@ -0,0 +1,44 @@
+import Extension from 'eon.extension.browser/extension';
+import Tabs from 'eon.extension.browser/tabs';
+import WebRequest from 'eon.extension.browser/web/request';
+
+import URI from 'urijs';
+
+import Log from 'eon.extension.framework/core/logger';
+
+
+/*
+ * Due to a bug with "web_accessible_resources" on firefox, we need to catch
+ * callback requests and redirect the tab to the correct page.
+ */
+export class Callback {
+    constructor() {
+        let callbackPattern = Extension.getCallbackPattern();
+
+        // Bind to callback requests
+        WebRequest.onBeforeRequest.addListener(
+            this.onBeforeRequest.bind(this),
+            {urls: [callbackPattern]}
+        );
+
+        Log.debug('Listening for callback requests matching "%s"...', callbackPattern);
+    }
+
+    onBeforeRequest(request) {
+        let url = new URI(request.url);
+
+        // Build destination url
+        let destinationUrl = new URI(Extension.getUrl(url.path()))
+            .search(url.search(true))
+            .toString();
+
+        // Navigate to callback page
+        Log.debug('Redirecting tab (id: %s) to extension callback', request.tabId);
+
+        Tabs.update(request.tabId, {
+            url: destinationUrl
+        });
+    }
+}
+
+export default new Callback();
