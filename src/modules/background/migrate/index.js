9cd41ccb9ed8d9a1c4ed77c22cf4a0321b6d0d36
Dean Gardiner <me@dgardiner.net>
2017-09-29 17:41:32 +1300

    Implemented preference/storage key migration

--- src/modules/background/migrate/index.js
+++ src/modules/background/migrate/index.js
@@ -1,3 +1,5 @@
+import ForEach from 'lodash-es/forEach';
+
 import Log from 'neon-extension-framework/core/logger';
 import Messaging from 'neon-extension-browser/messaging';
 import Registry from 'neon-extension-framework/core/registry';
@@ -8,8 +10,90 @@ import Plugin from '../../../core/plugin';
 
 export class Migrate {
     constructor() {
+        // Migrate preference/storage keys
+        this.migrateKeys();
+
         // Request legacy preferences
         Messaging.sendMessage({type: 'migrate/preferences'}, (preferences) => this.onPreferences(preferences));
+    }
+
+    migrateKeys() {
+        let keys = Object.keys(localStorage);
+
+        ForEach(keys, (key) => {
+            try {
+                // Migrate preference/storage key
+                if(key.startsWith('preferences:eon.extension')) {
+                    this.migratePreferenceKey(key, localStorage[key]);
+                } else if(key.startsWith('eon.extension')) {
+                    this.migrateStorageKey(key, localStorage[key]);
+                } else {
+                    return;
+                }
+            } catch(err) {
+                Log.warn('Unable to migrate "%s" key: %s', key, err.message, err);
+                return;
+            }
+
+            // Remove key
+            delete localStorage[key];
+        });
+    }
+
+    migratePreferenceKey(key, value) {
+        let targetKey = this.transformKey(key, 1);
+
+        // Ensure target key hasn't already been defined
+        if(isDefined(localStorage[targetKey])) {
+            Log.debug('Preference "%s" has already been defined', targetKey);
+            return;
+        }
+
+        // Store value
+        localStorage[targetKey] = value;
+    }
+
+    migrateStorageKey(key, value) {
+        let targetKey = this.transformKey(key, 0);
+
+        // Ensure target key hasn't already been defined
+        if(isDefined(localStorage[targetKey])) {
+            Log.debug('Value "%s" has already been defined', targetKey);
+            return;
+        }
+
+        // Store value
+        localStorage[targetKey] = value;
+    }
+
+    transformKey(key, position) {
+        let parts = key.split(':');
+
+        // Ensure context matches old format
+        if(!parts[position].startsWith('eon.extension')) {
+            throw new Error('Invalid context: ' + parts[position]);
+        }
+
+        // Transform context
+        parts[position] = this.transformContextName(parts[position]);
+
+        // Combine parts
+        return parts.join(':');
+    }
+
+    transformContextName(name) {
+        let parts = name.split('.');
+
+        // Ensure context matches old format
+        if(parts[0] !== 'eon' || parts[1] !== 'extension') {
+            throw new Error('Invalid context: ' + name);
+        }
+
+        // Update name
+        parts[0] = 'neon';
+
+        // Combine parts
+        return parts.join('-');
     }
 
     onPreferences(preferences) {
