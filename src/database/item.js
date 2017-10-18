680df7d4d9411e53e3a42f6165521ec7b66d9ac9
Dean Gardiner <me@dgardiner.net>
2017-10-19 11:16:33 +1300

    Updated item database methods to support model changes, and improved `upsert` item matching

--- src/database/item.js
+++ src/database/item.js
@@ -1,9 +1,10 @@
 import IsPlainObject from 'lodash-es/isPlainObject';
+import IsString from 'lodash-es/isString';
 import Map from 'lodash-es/map';
-import Merge from 'lodash-es/merge';
 
+import ItemBuilder from 'neon-extension-framework/models/item';
 import Log from 'neon-extension-core/core/logger';
-import {Track} from 'neon-extension-framework/models/item/music';
+import {Album, Artist, Track} from 'neon-extension-framework/models/item/music';
 import {isDefined} from 'neon-extension-framework/core/helpers';
 
 import Database from './base';
@@ -33,19 +34,37 @@ export class Items extends Database {
         options = options || {};
         options.force = options.force || false;
 
-        if(!isDefined(item) || !isDefined(item.ids)) {
-            return Promise.resolve(false);
+        if(!isDefined(item) || !isDefined(item.title)) {
+            return Promise.resolve({
+                created: false,
+                updated: false,
+
+                item
+            });
         }
 
         // Ignore items that have already been matched
         if(isDefined(item.id) && !item.changed && !options.force) {
-            return Promise.resolve(false);
+            return Promise.resolve({
+                created: false,
+                updated: false,
+
+                item
+            });
         }
 
         Log.trace('Upserting item: %o', item);
 
         // Build query
-        let query = this._buildUpsertQuery('ids', item.ids);
+        let query = this._createUpsertQuery(item);
+
+        if(!isDefined(query)) {
+            Log.error('Unable to create query for item: %o', item);
+
+            return Promise.reject(new Error(
+                'Unable to create query'
+            ));
+        }
 
         // Find items
         Log.trace('Finding items matching: %o', query);
@@ -56,13 +75,23 @@ export class Items extends Database {
                 item.id = result.docs[0]['_id'];
 
                 // Update item in database
-                return this.update(item);
+                return this.update(item).then((item) => ({
+                    created: false,
+                    updated: true,
+
+                    item
+                }));
             }
 
-            return this.create(item);
+            return this.create(item).then((item) => ({
+                created: true,
+                updated: false,
+
+                item
+            }));
         }, (err) => {
             // Unknown error
-            Log.warn('Unable to upsert item: %s', err.message, err);
+            Log.error('Unable to upsert item: %s', err.message, err);
             return Promise.reject(err);
         });
     }
@@ -71,18 +100,41 @@ export class Items extends Database {
         let updated = false;
 
         function resolve(result) {
-            if(result) {
+            if(result.updated) {
                 updated = true;
             }
+
+            return result.item;
         }
 
         if(item instanceof Track) {
             return Promise.resolve()
-                .then(() => this.upsert(item.artist).then(resolve))
-                .then(() => this.upsert(item.album.artist, { force: updated }).then(resolve))
-                .then(() => this.upsert(item.album, { force: updated }).then(resolve))
-                .then(() => this.upsert(item, { force: updated }).then(resolve))
-                .then(() => updated);
+                // Track (retrieve references)
+                .then(() => this.upsert(item, { force: updated }).then((result) => {
+                    item = result.item;
+                }))
+                // Artist
+                .then(() => this.upsert(item.artist, { force: updated }).then((result) => {
+                    item.artist = resolve(result);
+                }))
+                // Album Artist
+                .then(() => this.upsert(item.album.artist, { force: updated }).then((result) => {
+                    item.album.artist = resolve(result);
+                }))
+                // Album
+                .then(() => this.upsert(item.album, { force: updated }).then((result) => {
+                    item.album = resolve(result);
+                }))
+                // Track (set references)
+                .then(() => this.upsert(item, { force: updated }).then((result) => {
+                    item = resolve(result);
+                }))
+                // Result
+                .then(() => ({
+                    updated: updated,
+
+                    item
+                }));
         }
 
         return Promise.reject(new Error(
@@ -91,27 +143,17 @@ export class Items extends Database {
     }
 
     create(item) {
-        Log.trace('Creating item: %o', item);
+        Log.debug('Creating item: %o', item);
 
-        let data = IsPlainObject(item) ? item : item.toDocument();
-
-        let document = {
-            createdAt: Date.now(),
-            ...data,
-
-            seenAt: Date.now()
-        };
+        // Set timestamp
+        item.createdAt = Date.now();
 
         // Create item in database
-        return this.post(document).then((result) => {
+        return this.post(item.toDocument()).then((result) => {
             item.id = result.id;
-            item.createdAt = document.createdAt;
-            item.updatedAt = document.updatedAt;
-            item.seenAt = document.seenAt;
+            item.revision = result.rev;
 
-            item.changed = false;
-
-            return result;
+            return item;
         });
     }
 
@@ -128,9 +170,8 @@ export class Items extends Database {
 
             return {
                 createdAt: Date.now(),
-                ...data,
 
-                seenAt: Date.now()
+                ...data
             };
         });
 
@@ -153,7 +194,6 @@ export class Items extends Database {
                 item.id = result.id;
                 item.createdAt = doc.createdAt;
                 item.updatedAt = doc.updatedAt;
-                item.seenAt = doc.seenAt;
 
                 item.changed = false;
             }
@@ -163,36 +203,35 @@ export class Items extends Database {
     }
 
     update(item) {
-        Log.trace('Updating item: %o', item);
+        Log.trace('Comparing item: %o', item);
 
-        let data = IsPlainObject(item) ? item : item.toDocument();
-
-        // Update item in database
+        // Retrieve current item from database
         return this.get(item.id).then((doc) => {
-            let document = {
-                createdAt: Date.now(),
+            let current = ItemBuilder.decode(doc).merge(item);
 
-                ...doc,
-                ...data,
+            if(!current.changed) {
+                return Promise.resolve(current);
+            }
 
-                // Merge identifiers
-                ids: Merge(
-                    doc.ids || {},
-                    data.ids || {}
-                ),
+            // Update timestamps
+            if(!isDefined(current.createdAt)) {
+                current.createdAt = Date.now();
+            }
 
-                // Update timestamps
-                updatedAt: Date.now(),
-                seenAt: Date.now()
-            };
+            current.updatedAt = Date.now();
 
-            return this.put(document).then(() => {
-                item.createdAt = document.createdAt;
-                item.updatedAt = document.updatedAt;
-                item.seenAt = document.seenAt;
+            // Store item in database
+            Log.debug('Updating item: %o', current);
 
-                item.changed = false;
-                return true;
+            return this.put(current.toDocument()).then((result) => {
+                if(!result.ok) {
+                    return Promise.reject(new Error('' + 'Put failed'));
+                }
+
+                // Update values
+                current.revision = result.rev;
+
+                return current;
             });
         });
     }
@@ -216,8 +255,7 @@ export class Items extends Database {
                 ...data,
 
                 // Update timestamps
-                updatedAt: Date.now(),
-                seenAt: Date.now()
+                updatedAt: Date.now()
             };
         });
 
@@ -239,7 +277,6 @@ export class Items extends Database {
 
                 item.createdAt = doc.createdAt;
                 item.updatedAt = doc.updatedAt;
-                item.seenAt = doc.seenAt;
 
                 item.changed = false;
             }
@@ -248,32 +285,144 @@ export class Items extends Database {
         });
     }
 
-    _buildUpsertQuery(prefix, ids, query) {
-        query = query || {
-            fields: ['_id'],
-            selector: {$or: []}
-        };
+    _createUpsertQuery(item, fields) {
+        let selectors = this._createUpsertSelectors(item);
 
-        for(let key in ids) {
-            if(!ids.hasOwnProperty(key)) {
-                continue;
-            }
-
-            if(IsPlainObject(ids[key])) {
-                this._buildUpsertQuery(prefix + '.' + key, ids[key], query);
-                continue;
-            }
-
-            // Create selector
-            let selector = {};
-
-            selector[prefix + '.' + key] = {$eq: ids[key]};
-
-            // Add selector to OR clause
-            query.selector['$or'].push(selector);
+        // Ensure selectors exist
+        if(selectors.length < 1) {
+            return null;
         }
 
-        return query;
+        // Build query
+        return {
+            fields: [
+                '_id',
+
+                ...(fields || [])
+            ],
+            selector: {
+                $or: selectors
+            }
+        };
+    }
+
+    _createUpsertSelectors(item) {
+        if(isDefined(item.id)) {
+            return [{
+                '_id': item.id
+            }];
+        }
+
+        return [
+            ...this._createIdentifierSelectors(item),
+            ...this._createTreeSelector(item, 'title')
+        ];
+    }
+
+    _createIdentifierSelectors(item) {
+        let selectors = [];
+
+        function createSelectors(prefix, ids) {
+            for(let key in ids) {
+                if(!ids.hasOwnProperty(key)) {
+                    continue;
+                }
+
+                if(IsPlainObject(ids[key])) {
+                    createSelectors(prefix + '.' + key, ids[key]);
+                    continue;
+                }
+
+                // Create selector
+                let selector = {
+                    type: item.type
+                };
+
+                selector[prefix + '.' + key] = ids[key];
+
+                // Add selector to OR clause
+                selectors.push(selector);
+            }
+        }
+
+        // Create identifier selectors
+        createSelectors('ids', item.ids);
+
+        return selectors;
+    }
+
+    _createTreeSelector(item, key) {
+        let selector = {
+            'type': item.type
+        };
+
+        // Create item selectors
+        let valid;
+
+        if(item instanceof Track) {
+            valid = (
+                this._createItemSelector(selector, key, item) &&
+                this._createItemSelector(selector, key, 'album', item.album) &&
+                this._createItemSelector(selector, key, 'artist', item.artist)
+            );
+        } else if(item instanceof Album) {
+            valid = (
+                this._createItemSelector(selector, key, item) &&
+                this._createItemSelector(selector, key, 'artist', item.artist)
+            );
+        } else if(item instanceof Artist) {
+            valid = (
+                this._createItemSelector(selector, key, item)
+            );
+        } else {
+            return [];
+        }
+
+        // Ensure selector is valid
+        if(!valid) {
+            return [];
+        }
+
+        // Ensure selector has properties
+        if(Object.keys(selector).length < 1) {
+            return [];
+        }
+
+        return [selector];
+    }
+
+    _createItemSelector(selector, key, prefix, item) {
+        if(!IsString(prefix) && !isDefined(item)) {
+            item = prefix;
+            prefix = undefined;
+        }
+
+        if(!isDefined(item)) {
+            return false;
+        }
+
+        // Parse key
+        let name = key;
+
+        if(name === 'id') {
+            name = '_id';
+        }
+
+        // Parse prefix
+        if(!isDefined(prefix)) {
+            prefix = '';
+        } else {
+            prefix = prefix + '.';
+        }
+
+        // Ensure property exists
+        if(!isDefined(item[key])) {
+            return false;
+        }
+
+        // Create selector
+        selector[prefix + name] = item[key];
+        return true;
     }
 }
 
