b550f52f5124d479fa6bd0d9d01479cc22397600
Dean Gardiner <me@dgardiner.net>
2017-11-29 16:49:28 +1300

    Updated the `ItemDatabase.upsert` tests to cover slug matching

--- tests/database/item/upsert_tests.js
+++ tests/database/item/upsert_tests.js
@@ -18,7 +18,7 @@ describe('ItemDatabase', function() {
 
     describe('upsert', function() {
         it('upserts items', function() {
-            let item = Artist.create('test', {
+            let item = Artist.create('neon-extension-source-googlemusic', {
                 keys: {
                     id: 1
                 },
@@ -35,7 +35,7 @@ describe('ItemDatabase', function() {
                 expect(updated).toBe(false);
 
                 // Update item
-                return db.upsert(Artist.create('test', {
+                return db.upsert(Artist.create('neon-extension-source-googlemusic', {
                     keys: {
                         id: 1
                     },
@@ -54,12 +54,58 @@ describe('ItemDatabase', function() {
                     expect(current.createdAt).toBe(item.createdAt);
                     expect(current.updatedAt).toBeGreaterThan(item.updatedAt);
 
-                    expect(current.get('test').title).toBe('Gorillaz');
-                    expect(current.get('test').created).toBe(true);
-                    expect(current.get('test').updated).toBe(true);
+                    expect(current.get('neon-extension-source-googlemusic').title).toBe('Gorillaz');
+                    expect(current.get('neon-extension-source-googlemusic').created).toBe(true);
+                    expect(current.get('neon-extension-source-googlemusic').updated).toBe(true);
 
-                    expect(current.get('test').keys).toBeDefined();
-                    expect(current.get('test').keys.id).toBe(1);
+                    expect(current.get('neon-extension-source-googlemusic').keys).toBeDefined();
+                    expect(current.get('neon-extension-source-googlemusic').keys.id).toBe(1);
+                });
+            });
+        });
+
+        it('matches items by slug', function() {
+            let item = Artist.create('neon-extension-source-googlemusic', {
+                keys: {
+                    id: 1
+                },
+
+                title: 'Röyksopp'
+            });
+
+            // Create item
+            return db.upsert(item).then(({created, updated}) => {
+                expect(created).toBe(true);
+                expect(updated).toBe(false);
+
+                expect(item.keys['item'].slug).toBe('royksopp');
+                expect(item.title).toBe('Röyksopp');
+
+                expect(item.get('neon-extension-source-googlemusic').keys.id).toBe(1);
+                expect(item.get('neon-extension-source-googlemusic').title).toBe('Röyksopp');
+
+                // Update item
+                return db.upsert(Artist.create('neon-extension-destination-lastfm', {
+                    keys: {
+                        id: 1
+                    },
+
+                    title: 'Royksopp'
+                })).then(({created, updated, item: current}) => {
+                    expect(created).toBe(false);
+                    expect(updated).toBe(true);
+
+                    expect(current.id).toBe(item.id);
+                    expect(current.revision).not.toBe(item.revision);
+
+                    expect(current.keys['item'].slug).toBe('royksopp');
+                    expect(current.title).toBe('Röyksopp');
+
+                    expect(current.get('neon-extension-source-googlemusic').keys.id).toBe(1);
+                    expect(current.get('neon-extension-source-googlemusic').title).toBe('Röyksopp');
+
+                    expect(current.get('neon-extension-destination-lastfm').keys.id).toBe(1);
+                    expect(current.get('neon-extension-destination-lastfm').title).toBe('Royksopp');
                 });
             });
         });
