adb3f3c3a5fa0355c9768cc9bacc69217ad77ed1
Dean Gardiner <me@dgardiner.net>
2018-05-13 16:03:26 +1200

    Updated logger to support the new consolidated "Log Level" option

--- src/services/configuration/options.js
+++ src/services/configuration/options.js
@@ -1,28 +1,10 @@
-import {
-    Group,
-    Page,
-    SelectOption
-} from 'neon-extension-framework/services/configuration/models';
+import {Group, Page} from 'neon-extension-framework/services/configuration/models';
 
 import LoggerLevelOption from './models/logger/level';
 import Plugin from '../../core/plugin';
 
 
 export default [
-    new Page(Plugin, 'general', 'General', [
-        new Group(Plugin, 'debugging', 'Debugging', [
-            new SelectOption(Plugin, 'log_level', 'Log Level', [
-                {key: 'error', label: 'Error'},
-                {key: 'warning', label: 'Warning'},
-                {key: 'notice', label: 'Notice'},
-                {key: 'info', label: 'Info'},
-                {key: 'debug', label: 'Debug'},
-                {key: 'trace', label: 'Trace'}
-            ], {
-                default: 'warning'
-            })
-        ])
-    ]),
     new Page(Plugin, 'debugging', 'Debugging', [
         new Group(Plugin, 'logging', 'Logging', [
             new LoggerLevelOption(Plugin, 'log_level', 'Log Level')
