0c1307aa7c91bf00b018c6376dc91efe151df849
Dean Gardiner <me@dgardiner.net>
2018-05-17 17:23:46 +1200

    Updated the settings view to use locale namespaces

--- src/modules/configuration/views/settings.js
+++ src/modules/configuration/views/settings.js
@@ -1,10 +1,14 @@
+import ClassNames from 'classnames';
+import IsEqual from 'lodash-es/isEqual';
 import IsNil from 'lodash-es/isNil';
 import React from 'react';
 
 import Log from 'neon-extension-core/core/logger';
 import Preferences from 'neon-extension-framework/preferences';
+import {EnableOption} from 'neon-extension-framework/services/configuration/models';
 
 import Group from '../components/settings/group';
+import I18n from '../components/I18n';
 import './settings.scss';
 
 
@@ -13,7 +17,12 @@ export default class Options extends React.Component {
         super(props);
 
         this.state = {
-            page: {}
+            enabled: true,
+
+            page: null,
+            headerOption: null,
+
+            namespaces: []
         };
     }
 
@@ -26,30 +35,32 @@ export default class Options extends React.Component {
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        if(nextProps.type !== this.state.type) {
-            return true;
-        }
+        return (
+            // Properties
+            !IsEqual(this.props.params, nextProps.params) ||
 
-        if(nextProps.pageId !== this.state.pageId) {
-            return true;
-        }
+            // State
+            !IsEqual(this.state.namespaces, nextState.namespaces) ||
+            this.state.enabled !== nextState.enabled ||
+            this.state.page !== nextState.page ||
+            this.state.headerOption !== nextState.headerOption
+        );
+    }
 
-        return false;
+    onEnableChanged(enabled) {
+        this.setState({
+            enabled: enabled
+        });
     }
 
     refresh(props) {
         let type = props.params.type || 'core';
-        let pluginId = props.params.pluginId || 'neon-extension';
-        let key = props.params.key || null;
 
-        // Build page identifier
-        let pageId;
-
-        if(!IsNil(key)) {
-            pageId = pluginId + ':' + key;
-        } else {
-            pageId = pluginId;
-        }
+        // Retrieve page identifiers
+        let pageId = this._getPageIdentifier(
+            props.params.pluginId || 'neon-extension',
+            props.params.key || null
+        );
 
         // Try find matching preference page
         let page = Preferences.pages[type][pageId];
@@ -59,23 +70,110 @@ export default class Options extends React.Component {
             return;
         }
 
+        // Build locale namespaces
+        let namespaces = [];
+
+        if(!IsNil(page)) {
+            namespaces = [
+                page.namespace,
+                page.plugin.namespace
+            ];
+        }
+
         // Update state
         this.setState({
-            type: type || null,
-            pageId: pageId || null,
-            page: page || {}
+            enabled: true,
+
+            page: page || null,
+            headerOption: this._getHeaderOption(page.children),
+
+            namespaces
+        });
+
+        // Refresh enable option
+        this.refreshEnableOption();
+    }
+
+    refreshEnableOption() {
+        if(IsNil(this.state.headerOption)) {
+            return;
+        }
+
+        if(!(this.state.headerOption instanceof EnableOption)) {
+            return;
+        }
+
+        // Fetch current state
+        this.state.headerOption.isEnabled().then((enabled) => {
+            // Update state
+            this.setState({
+                enabled
+            });
         });
     }
 
     render() {
         return (
-            <div data-view="neon-extension-core:settings" className="options">
-                <Group
-                    type="flat"
-                    title={this.state.page.title}
-                    children={this.state.page.children}
-                />
-            </div>
+            <I18n ns={this.state.namespaces} nsMode="fallback">
+                {(t) => (
+                    <div data-view="neon-extension-core:settings"
+                        className={ClassNames('group group-flat row', {'enabled': this.state.enabled})}>
+                        <div className="header columns">
+                            <div className={ClassNames('header-inner', 'row')}>
+                                <div className="header-title small-10 columns">
+                                    <h3>{t('title')}</h3>
+                                </div>
+
+                                {this.state.headerOption &&
+                                    <div className="header-option small-2 columns" style={{ textAlign: 'right' }}>
+                                        {this.state.headerOption.type === 'enable' &&
+                                            Group.renderItem(this.state.headerOption, this.onEnableChanged.bind(this))
+                                        }
+                                    </div>
+                                }
+                            </div>
+                        </div>
+
+                        {this.state.page && this.state.page.children && <div className="content columns">
+                            <div className="children row">
+                                {this.state.page.children.map((item) => {
+                                    if(item.type === 'enable') {
+                                        return null;
+                                    }
+
+                                    return Group.renderItem(item);
+                                })}
+                            </div>
+
+                            <div className="overlay"/>
+                        </div>}
+                    </div>
+                )}
+            </I18n>
         );
+    }
+
+    _getHeaderOption(children) {
+        for(let i = 0; i < children.length; ++i) {
+            let item = children[i];
+
+            if(item.type === 'enable') {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    _getPageIdentifier(pluginId, key = null) {
+        let pageId;
+
+        if(!IsNil(key)) {
+            pageId = `${pluginId}:${key}`;
+        } else {
+            pageId = pluginId;
+        }
+
+        return pageId;
     }
 }
