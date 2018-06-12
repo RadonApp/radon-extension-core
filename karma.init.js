44964e85b64eb150d034cef7132218ed0709814c
Dean Gardiner <me@dgardiner.net>
2018-06-12 18:14:49 +1200

    Reduced karma memory usage
    This should fix segmentation faults on Travis CI

--- karma.init.js
+++ karma.init.js
@@ -1,2 +1,16 @@
 /* globals jasmine */
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30 * 1000;
+
+// Import all modules in context
+function importAll(r) {
+    r.keys().forEach(r);
+}
+
+// Import source directories
+importAll(require.context('./App', true, /.js$/));
+importAll(require.context('./Components', true, /.js$/));
+importAll(require.context('./Core', true, /.js$/));
+importAll(require.context('./Database', true, /.js$/));
+importAll(require.context('./Messaging', true, /.js$/));
+importAll(require.context('./Models', true, /.js$/));
+importAll(require.context('./Services', true, /.js$/));
